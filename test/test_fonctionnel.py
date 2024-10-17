7ebdf730e239d779100ca5b161d2ac6f142d59f3
Mollylil16 <mollylil663@gmail.com>
2024-10-17 16:09:17 +0100

    flask test3

--- test/test_fonctionnel.py
+++ test/test_fonctionnel.py
@@ -9,10 +9,10 @@ def client():
 def test_book(client):
     response = client.get('/book/Spring Festival/Simply Lift')
     assert response.status_code == 200
-    assert b'Booking for Spring Festival' in response.data
+    assert b'Booking for Spring Festival' in response.data  
 
 def test_purchase_places(client):
-    
+    # Créer un club dans la session pour tester l'achat de places
     client.post('/showSummary', data={'email': 'john@simplylift.co'})
     
     response = client.post('/purchasePlaces', data={
@@ -21,4 +21,6 @@ def test_purchase_places(client):
         'places': '3'
     })
     assert response.status_code == 200
+    
+    # Changez cette ligne pour décoder la réponse en UTF-8
     assert b'Super, réservation terminée!' in response.data
